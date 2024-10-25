e60762b70da5bde9e2a031ad31bf52386e25ed0a
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-10-25 11:35:52 +0700

    basic boilerplate

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import { Server } from 'socket.io';
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+io.on('connection', (socket) => {
+  console.log('A socket connected', 'Socket Id: ', socket.id );
+  socket.on('disconnect', () => {
+    console.log('A socket disconnected', 'Socket Id: ', socket.id);
+  });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
