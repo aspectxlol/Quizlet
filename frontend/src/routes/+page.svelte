5d0bb999a8a986e958695ffc4912a7556680557a
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-10-25 09:36:31 +0700

    i am infact at shcool, also docker build for frontend passed

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<head>
+  <title>Quizlet</title>
+</head>
+
+<div class="flex flex-col items-center justify-center h-screen">
+  <h1 class="text-9xl text-center font-bold">Hello, Welcome to my little site!</h1>
+</div>
