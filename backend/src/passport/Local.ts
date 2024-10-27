a19568b60d861a6e94dab9b7d726745079cbe0d2
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-10-27 15:07:25 +0700

    backend init again

--- backend/src/passport/Local.ts
+++
@@ -1,8 +0,0 @@
-import passport from 'passport';
-import { Strategy } from 'passport-local';
-import { User } from '../db/schema.js';
-
-export const localStrategy = new Strategy({
-  usernameField: 'username',
-  passwordField: 'password',
-}, async (username, password, done) => {});
